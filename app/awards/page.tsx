017f3050204f0f55e6fce1b00f6cecaca125bb26
jiwonlee1223 <to55540750@gmail.com>
2025-02-12 16:21:13 +0900

    Awards 페이지 완료

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -1,15 +1,141 @@
 export default function AwardsPage() {
-    return (
-        <div className="main-page-container">
-      <div className="half">
-        <h1>Awards</h1>
+  return (
+    <>
+      <div className="main-page-container">
+        <div className="half"></div>
+        <div className="halftwo">
+          <span className="awards-year">2024</span>
+        </div>
+        <div className="halfthree">
+        </div>
       </div>
-      <div className="half">
-        <p className="comingsoon">
-          Coming Soon
-        </p>
+
+      <div className="main-page-container">
+        <div className="half"></div>
+        <div className="halftwo">
+          <img src="/angelgear.png" className="awards-img" />
+        </div>
+        <div className="halfthree">
+          <div className="award-description-container">
+            <span className="awards-title">angel GEAR: An Exoskeleton for Industrial Safety</span> <br />
+            <span className="awards-subtitle">Exoskeleton for Industrial Safety</span> <br />
+            <div className="awards-tag-container">
+              <img src="/if2024.png" className="awards-tag" /> <br />
+              <img src="/reddot2024.png" className="awards-tag" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-      );
-  }
-  
+
+      <div className="main-page-container">
+        <div className="awards-blank"></div>
+      </div>
+
+      <div className="main-page-container">
+        <div className="half"></div>
+        <div className="halftwo">
+          <span className="awards-year">2022</span>
+        </div>
+        <div className="halfthree">
+        </div>
+      </div>
+
+      <div className="main-page-container">
+        <div className="half"></div>
+        <div className="halftwo">
+          <img src="/mcm.png" className="awards-img" />
+        </div>
+        <div className="halfthree">
+          <div className="award-description-container">
+            <span className="awards-title">MCM SHUTTER</span> <br />
+            <span className="awards-subtitle">Infected Waste-Water Management Device</span> <br />
+            <div className="awards-tag-container">
+              <img src="/if2021.png" className="awards-tag" /> <br />
+              <img src="/reddot2022.png" className="awards-tag" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="main-page-container">
+        <div className="awards-blank"></div>
+      </div>
+
+      <div className="main-page-container">
+        <div className="half"></div>
+        <div className="halftwo">
+          <span className="awards-year">2021</span>
+        </div>
+        <div className="halfthree">
+        </div>
+      </div>
+
+      <div className="main-page-container">
+        <div className="half"></div>
+        <div className="halftwo">
+          <img src="/kare.png" className="awards-img" />
+        </div>
+        <div className="halfthree">
+          <div className="award-description-container">
+            <span className="awards-title">KARE : Mobile Clinic Module</span> <br />
+            <span className="awards-subtitle">Mobile clinic module</span> <br />
+            <div className="awards-tag-container">
+              <img src="/if2021.png" className="awards-tag" /> <br />
+              <img src="/reddot2021best.png" className="awards-tag" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="main-page-container">
+        <div className="half"></div>
+        <div className="halftwo">
+          <img src="/karemcm.png" className="awards-img" />
+        </div>
+        <div className="halfthree">
+          <div className="award-description-container">
+            <span className="awards-title">KARE MCM</span> <br />
+            <span className="awards-subtitle">Medical Equipment Control UI/UX</span> <br />
+            <div className="awards-tag-container">
+              <img src="/if2021.png" className="awards-tag" /> <br />
+              <img src="/reddot2021best.png" className="awards-tag" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="main-page-container">
+        <div className="half"></div>
+        <div className="halftwo">
+          <img src="/toque.png" className="awards-img" />
+        </div>
+        <div className="halfthree">
+          <div className="award-description-container">
+            <span className="awards-title">TOQUE</span> <br />
+            <span className="awards-subtitle">Covid-19 pass box</span> <br />
+            <div className="awards-tag-container">
+              <img src="/if2021.png" className="awards-tag" /> <br />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="main-page-container">
+        <div className="half"></div>
+        <div className="halftwo">
+          <img src="/iotizer.png" className="awards-img" />
+        </div>
+        <div className="halfthree">
+          <div className="award-description-container">
+            <span className="awards-title">IoTIZER</span> <br />
+            <span className="awards-subtitle">IoT interactive gadget</span> <br />
+            <div className="awards-tag-container">
+              <img src="/if2021.png" className="awards-tag" /> <br />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+
+  );
+}
