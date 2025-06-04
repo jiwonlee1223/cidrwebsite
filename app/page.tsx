f5ac00d16a9ab25e65bd493d4728a8dfe05d0bfa
jiwonlee1223 <to55540750@gmail.com>
2025-06-04 18:10:56 +0900

    Publish 3차 완료

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 export default function MainPage() {
   return (
     <>
+    <div className="resources-blank"> </div>
       {/* <div className="main-banner">
         <video
           className="banner"
